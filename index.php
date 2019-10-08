645567c53051e98156403dc24efe76c2c7255cfa
Nicolai Mortensen <nico644a@edu.campusvejle.dk>
2019-10-08 11:37:50 +0200

    Update

--- index.php
+++ index.php
@@ -1,13 +1,151 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    
-    <title>Document</title>
+  <head>
+    <title>Title</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="styles\styles.css">
 </head>
-<body>
-    
+  <body class="container">
+      <div class="row">
+
+         <header class="col-12"></header>
+
+         <nav class="navbar navbar-expand-sm navbar-inverse col-12">
+            <div class="container">
+                <button class="navbar-toggler" data-toggle="collapse" data-target="#mainNav">
+                    <span class="navbar-toggler-icon bg-info"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="mainNav">
+                    <div class="navbar-nav text-inline">
+                        <ul>
+                        <a class="nav-item nav-link active" href="#"><li>Home Page</li></a>
+                        <a class="nav-item nav-link" href="#"><li></li>Log In</li></a>
+                        <a class="nav-item nav-link" href="#"><li></li>About Us</li></a>
+                    </ul>
+                    </div>
+                </div>
+            </div>
+        </nav>
+
+        <article>
+            <h2></h2>
+            <img src="" alt="">
+            <p class="Sail-Text"></p>
+            <br>
+            <p class="Price"></p>
+            <br>
+            <p class="Category"></p>
+            <br>
+            <p class="Publish-Date"></p>
+            <br>
+            <p class="Seller"></p>
+            <br><button>Buy</button>
+        </article>
+        <article>
+            <h2></h2>
+            <img src="" alt="">
+            <p class="Sail-Text"></p>
+            <br>
+            <p class="Price"></p>
+            <br>
+            <p class="Category"></p>
+            <br>
+            <p class="Publish-Date"></p>
+            <br>
+            <p class="Seller"></p>
+            <br><button>Buy</button>
+        </article>
+        <article>
+            <h2></h2>
+            <img src="" alt="">
+            <p class="Sail-Text"></p>
+            <br>
+            <p class="Price"></p>
+            <br>
+            <p class="Category"></p>
+            <br>
+            <p class="Publish-Date"></p>
+            <br>
+            <p class="Seller"></p>
+            <br><button>Buy</button>
+        </article>
+        <article>
+            <h2></h2>
+            <img src="" alt="">
+            <p class="Sail-Text"></p>
+            <br>
+            <p class="Price"></p>
+            <br>
+            <p class="Category"></p>
+            <br>
+            <p class="Publish-Date"></p>
+            <br>
+            <p class="Seller"></p>
+            <br><button>Buy</button>
+        </article>
+        <article>
+            <h2></h2>
+            <img src="" alt="">
+            <p class="Sail-Text"></p>
+            <br>
+            <p class="Price"></p>
+            <br>
+            <p class="Category"></p>
+            <br>
+            <p class="Publish-Date"></p>
+            <br>
+            <p class="Seller"></p>
+            <br><button>Buy</button>
+        </article>
+        <article>
+            <h2></h2>
+            <img src="" alt="">
+            <p class="Sail-Text"></p>
+            <br>
+            <p class="Price"></p>
+            <br>
+            <p class="Category"></p>
+            <br>
+            <p class="Publish-Date"></p>
+            <br>
+            <p class="Seller"></p>
+            <br><button>Buy</button>
+        </article>
+        <article>
+            <h2></h2>
+            <img src="" alt="">
+            <p class="Sail-Text"></p>
+            <br>
+            <p class="Price"></p>
+            <br>
+            <p class="Category"></p>
+            <br>
+            <p class="Publish-Date"></p>
+            <br>
+            <p class="Seller"></p>
+            <br><button>Buy</button>
+        </article>
+        <article>
+            <h2></h2>
+            <img src="" alt="">
+            <p class="Sail-Text"></p>
+            <br>
+            <p class="Price"></p>
+            <br>
+            <p class="Category"></p>
+            <br>
+            <p class="Publish-Date"></p>
+            <br>
+            <p class="Seller"></p>
+            <br><button>Buy</button>
+        </article>
+
+    </div>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <script src="javascript\myJavascript.js">
 </body>
 </html>
